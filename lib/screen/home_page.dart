3e9c87d274d0eadb0c884b38660517bd91aa3f3d
Jonatas Santos <jonatas.santos@unicornio.academy>
2022-08-22 10:24:14 -0300

    transformation example

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -8,13 +8,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double _x = 0.0;
+  double _y = 0.0;
+  double _z = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Transfomations Example'),
       ),
-      body: Container(),
+      body: Container(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [const Text('X'), Slider(value: _x, onChanged: (double value) => setState(() => _x = value))],
+              ),
+              Row(
+                children: [const Text('Y'), Slider(value: _y, onChanged: (double value) => setState(() => _y = value))],
+              ),
+              Row(
+                children: [const Text('Z'), Slider(value: _z, onChanged: (double value) => setState(() => _z = value))],
+              ),
+              Transform(
+                transform: Matrix4.skewY(_y),
+                child: Transform(
+                  transform: Matrix4.skewX(_x),
+                  child: Transform(
+                    transform: Matrix4.rotationZ(_z),
+                    child: const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text('hello World'),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
